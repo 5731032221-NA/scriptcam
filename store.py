754fe5460b07f721ea6e361b0ef198859a1fd14c
msc-dts <naphol159@gmail.com>
2020-04-10 06:21:46 +0700

    first commit

--- store.py
+++ store.py
@@ -50,11 +50,11 @@ def storeblob(name):
 
 def imagescan(frame, count):
     # print("cc",count)
-    if (count % 30) == 0:
+    if (count % 15) == 0:
         now=datetime.now() + timedelta(hours=7)
         today=date.today() + timedelta(hours=7)
         current_time=now.strftime("%H%M%S")
-        name=str(today)+"-1-"+current_time+"test.jpg"
+        name=str(today)+"-1-"+current_time+str(count % 30)+"test.jpg"
         print("count",count)
         cv2.imwrite("data/"+name, frame)
         storeblob(name)
