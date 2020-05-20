964ed2c4ebae3c6545492399556d490d1ab49cc2
msc-dts <naphol159@gmail.com>
2020-05-20 09:14:29 +0700

    store

--- scriptCam2.py
+++ scriptCam2.py
@@ -234,11 +234,11 @@ def mongo(now,timei, nameperson, checkin, faceAttributes, faceRectangle, image_u
         db_default = client.mea
         query_default = {"id": nameperson}
         default_data = db_default.default.find_one(query_default)
-        newvalues = { "$set": { "cameraout": 2,"checkout": checkin ,"checkoutEmotion": { "gender": default_data['gender'], "age": faceAttributes['age']+ int(default_data['margin']), "emotion": faceAttributes['emotion'] },"checkoutEmo": emo, "checkoutImageCrop": imageCropUrl , "checkoutdatetime": now.strftime("%Y%m%d%H%M%S")} }
+        newvalues = { "$set": { "cameraout": 2,"checkout": checkin ,"checkoutEmotion": { "gender": default_data['gender'], "age": faceAttributes['age']+ int(default_data['margin']), "emotion": faceAttributes['emotion'] },"checkoutEmo": "neutral", "checkoutImageCrop": imageCropUrl , "checkoutdatetime": now.strftime("%Y%m%d%H%M%S")} }
         db.checkin[today].update_one(query, newvalues)
 
         db.checkattendance.update_one(query, { "$set": {"checkout": { "time": now.strftime("%H:%M:%S"),
-                    "emotion": { "gender": default_data['gender'], "age": faceAttributes['age']+ int(default_data['margin']), "emotion": faceAttributes['emotion'] }
+                    "emotion": { "gender": default_data['gender'], "age": faceAttributes['age']+ int(default_data['margin']), "emotion": "neutral" }
         }} }) 
 
 def mongo2(now,timei, nameperson, checkin, faceRectangle, image_url, imageCropUrl):
@@ -247,7 +247,7 @@ def mongo2(now,timei, nameperson, checkin, faceRectangle, image_url, imageCropUr
     client = pymongo.MongoClient(
             "mongodb://127.0.0.1:27017")
     db = client.checkin
-    emo = getemo(faceAttributes['emotion'])
+    # emo = getemo(faceAttributes['emotion'])
     query = {"id": nameperson}
     queryy = db.checkin[today].find(query)
     # # ##print(queryy.count())
