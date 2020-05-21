f836aaa572eb1712abbb2566cbfc1a9dbd6cd96a
msc-dts <naphol159@gmail.com>
2020-05-21 16:23:41 +0700

    update

--- scriptCam3.py
+++ scriptCam3.py
@@ -358,6 +358,7 @@ def imagescan(frame, count):
         #time.sleep(count/60)
         # frame=resize(img)
         # frame = img
+        sent = 0
         gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces=face_cascade.detectMultiScale(gray, 1.1, 4)
         for (x, y, w, h) in faces:
@@ -378,7 +379,7 @@ def imagescan(frame, count):
                 cv2.imwrite("data/"+name, frame)
 
                 storeblob(name)
-
+                sent = 1
                 response=apidetect(name)
                 detect=response.json()
                 
@@ -446,7 +447,91 @@ def imagescan(frame, count):
                             os.remove("data/"+name_crop)
 
                 os.remove("data/"+name)
+        if(sent == 0):
+            body = frontalface_alt.detectMultiScale(img,1.08,1 )
+            for (x, y, w, h) in body:
+                cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        #         cv2.imwrite('my.jpg', img)
+                roi_gray = gray[y:y+h, x:x+w]
+                roi_color = img[y:y+h, x:x+w]
+                eyel = face_cascade.detectMultiScale(roi_gray,1.08,)
+                if((eyel is not ())):
+                    now=datetime.now() + timedelta(hours=7)
+                # today=date.today() + timedelta(hours=7)
+                    current_time=now.strftime("%H%M%S")
+                    name=now.strftime("%Y-%m-%d")+"-3-"+current_time+str(count%60)+".jpg"
+                    cv2.imwrite("data/"+name, frame)
+
+                    storeblob(name)
+                    
+                    response=apidetect(name)
+                    detect=response.json()
+                    
+                    if(detect != []):
+                        arrfaceid=[]
+                        for face in detect:
+                            arrfaceid.append(face[u'faceId'])
+                        response=apiidentify(name, arrfaceid)
+                        identify=response.json()
+                        for index, iden in enumerate(identify):
+                            uriPerson='https://meafacedetection.cognitiveservices.azure.com/face/v1.0/persongroups/mea/persons/' + \
+                                str(json.dumps(identify[index][u'candidates'][0][u'personId'])).replace(
+                                    '"', '')
+                            header={'Ocp-Apim-Subscription-Key': subscription_key}
+                            crop_img=frame[list(detect[index][u'faceRectangle'].values())[0]: (list(detect[index][u'faceRectangle'].values())[0] + list(detect[index][u'faceRectangle'].values())[
+                                                3]), list(detect[index][u'faceRectangle'].values())[1]:(list(detect[index][u'faceRectangle'].values())[1] + list(detect[index][u'faceRectangle'].values())[2])]
+                            name_crop=now.strftime("%Y-%m-%d")+"-3-"+current_time+str(count%60)+"-crop.jpg"
+                            cv2.imwrite("data/"+name_crop, crop_img)
+                            storecrop(name_crop,now)
+                            if(identify[index][u'candidates'][0][u'confidence'] > 0.5):
         
+                                person=requests.get(uriPerson,  headers = header)
+                                nameperson=person.json()[u'name']
+
+                                mongo(now,now.strftime("%H:%M"), nameperson, now.strftime("%H:%M"), detect[index][u'faceAttributes'], detect[index][u'faceRectangle'], (
+                                    "https://oneteamblob.blob.core.windows.net/facedetection/"+name), name_crop)
+                                # mongo(now,now.strftime("%H:%M"), nameperson, now.strftime("%H:%M"), detect[index][u'faceRectangle'], (
+                            #      "https://oneteamblob.blob.core.windows.net/facedetection/"+name), name_crop)
+                                infocrop(name_crop,now,nameperson) 
+                            else:
+                                infocrop(name_crop,now,"") 
+                            os.remove("data/"+name_crop)
+                    else:
+                        response=apidetect2(name)
+                        detect=response.json()
+                        
+                        if(detect != []):
+                            arrfaceid=[]
+                            for face in detect:
+                                arrfaceid.append(face[u'faceId'])
+                            response=apiidentify(name, arrfaceid)
+                            identify=response.json()
+                            for index, iden in enumerate(identify):
+                                uriPerson='https://meafacedetection.cognitiveservices.azure.com/face/v1.0/persongroups/mea/persons/' + \
+                                    str(json.dumps(identify[index][u'candidates'][0][u'personId'])).replace(
+                                        '"', '')
+                                header={'Ocp-Apim-Subscription-Key': subscription_key}
+                                crop_img=frame[list(detect[index][u'faceRectangle'].values())[0]: (list(detect[index][u'faceRectangle'].values())[0] + list(detect[index][u'faceRectangle'].values())[
+                                                    3]), list(detect[index][u'faceRectangle'].values())[1]:(list(detect[index][u'faceRectangle'].values())[1] + list(detect[index][u'faceRectangle'].values())[2])]
+                                name_crop=now.strftime("%Y-%m-%d")+"-3-"+current_time+str(count%60)+"-crop.jpg"
+                                cv2.imwrite("data/"+name_crop, crop_img)
+                                storecrop(name_crop,now)
+                                if(identify[index][u'candidates'][0][u'confidence'] > 0.5):
+        
+                                    person=requests.get(uriPerson,  headers = header)
+                                    nameperson=person.json()[u'name']
+
+                                    # mongo(now,now.strftime("%H:%M"), nameperson, now.strftime("%H:%M"), detect[index][u'faceAttributes'], detect[index][u'faceRectangle'], (
+                                    #     "https://oneteamblob.blob.core.windows.net/facedetection/"+name), name_crop)
+                                    mongo2(now,now.strftime("%H:%M"), nameperson, now.strftime("%H:%M"), detect[index][u'faceRectangle'], (
+                                        "https://oneteamblob.blob.core.windows.net/facedetection/"+name), name_crop)
+                                    infocrop(name_crop,now,nameperson) 
+                                else:
+                                    infocrop(name_crop,now,"") 
+                                os.remove("data/"+name_crop)
+
+                    os.remove("data/"+name)
+                    
 now=datetime.now()
 
 current_time=now.strftime("%H%M%S")
