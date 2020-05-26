d27c816730d67d8b162e084bec6db2bcb76a0db5
msc-dts <naphol159@gmail.com>
2020-05-26 22:20:20 +0700

    a

--- scriptCam4.py
+++ scriptCam4.py
@@ -240,8 +240,9 @@ def mongo(now,timei, nameperson, checkin, faceAttributes, faceRectangle, image_u
         # else: 
         db_default = client.mea
         query_default = {"id": nameperson}
+        default_data = db_default.default.find_one(query_default)
         if(default_data['gender'] == faceAttributes['gender']):
-            default_data = db_default.default.find_one(query_default)
+            
             newvalues = { "$set": { "cameraout": 4,"checkout": checkin ,"checkoutEmotion": { "gender": default_data['gender'], "age": faceAttributes['age']+ int(default_data['margin']), "emotion": faceAttributes['emotion'] },"checkoutEmo": emo, "checkoutImageCrop": imageCropUrl , "checkoutdatetime": now.strftime("%Y%m%d%H%M%S")} }
             db.checkin[today].update_one(query, newvalues)
 
