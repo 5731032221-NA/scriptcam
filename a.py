ed8f6c578dbbad9d5b38da0afee43536927bd09f
msc-dts <naphol159@gmail.com>
2020-05-26 22:26:46 +0700

    a

---
+++ a.py
@@ -0,0 +1,34 @@
+import numpy as np
+import cv2
+import os
+import uuid
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+import pymongo
+from Crypto.Cipher import AES
+from Crypto import Random
+import base64
+# import io
+from io import BytesIO
+from PIL import Image
+from pkcs7 import PKCS7Encoder
+
+# from io import StringIO
+# import sys
+# import os
+
+# sys.stdout = open(os.devnull, "w")
+
+# import asyncio
+
+# import concurrent.futures
+import _thread
+
+import requests
+import json
+from datetime import  timedelta, datetime, date, time as t2
+import time
+from random import randint
+
+timenow =datetime.now() + timedelta(hours=7)
+print(((int(t2(5,00).strftime("%H%M"))>int( (timenow).strftime("%H%M")) ) | (int(t2(20,00).strftime("%H%M"))<int( (timenow).strftime("%H%M")) )  ))
+print((timenow).weekday() < 5)
