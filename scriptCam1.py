f6d5ecdcb0ade48ab8603c7cf1c0b6e4d3a91ccb
msc-dts <naphol159@gmail.com>
2020-05-28 06:22:01 +0700

    a

--- scriptCam1.py
+++ scriptCam1.py
@@ -661,7 +661,7 @@ count1=1
 while(True):
     ret, img=cap.read()
     timenow =datetime.now() + timedelta(hours=7)
-    if (((int(t2(5,00).strftime("%H%M"))<int( (timenow).strftime("%H%M")) ) & (int(t2(20,00).strftime("%H%M"))>int( (timenow).strftime("%H%M")) )  ) & ((timenow).weekday() < 5)):
+    if (cv2.waitKey(20) & 0xFF == ord('q')):
        break
     else:
         if ret:
