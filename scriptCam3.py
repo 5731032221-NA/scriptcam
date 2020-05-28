94d35e490b8134a0e1420aa4169f624d9bd092c9
msc-dts <naphol159@gmail.com>
2020-05-28 13:14:28 +0700

    a

--- scriptCam3.py
+++ scriptCam3.py
@@ -100,8 +100,8 @@ cap = cv2.VideoCapture("rtsp://admin:admin@10.76.53.16:8554/stream0/out.h264")
 
 def resize(img):
     scale_percent = 80  # percent of original size
-    width = int(img.shape[1] * scale_percent / 80)
-    height = int(img.shape[0] * scale_percent / 80)
+    width = int(img.shape[1] * scale_percent / 50)
+    height = int(img.shape[0] * scale_percent / 50)
     dim = (width, height)
 
     return cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
