c600d26ab1237a681a73d17b43168b226ba28e2d
msc-dts <naphol159@gmail.com>
2020-06-14 10:18:25 +0700

    update

--- sc1v2.py
+++ sc1v2.py
@@ -723,8 +723,8 @@ while(True):
         ret, img=cap.read()
         timenow =datetime.now() + timedelta(hours=7)
         bool1 = ((int(t2(5,00).strftime("%H%M"))<int( (timenow).strftime("%H%M")) ) & (int(t2(20,00).strftime("%H%M"))>int( (timenow).strftime("%H%M")) )  ) & ((timenow).weekday() < 5)
-        # bool1 = True
-        bool1 = False
+        bool1 = True
+        # bool1 = False
         if ((cv2.waitKey(20) & 0xFF == ord('q')) | (not bool1)):
         # if ((cv2.waitKey(20) & 0xFF == ord('q'))):
             break
