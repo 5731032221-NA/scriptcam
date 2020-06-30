daed35b234de3c91f0893a658fbfa2afee693dfe
msc-dts <naphol159@gmail.com>
2020-06-30 14:17:02 +0700

    a

--- sc1v2.py
+++ sc1v2.py
@@ -747,7 +747,7 @@ count1=1
 print("new running")
 while(True):
     print("connect camera...")
-    cap = cv2.VideoCapture("rtsp://admin:admin@10.76.53.14:8554/stream0/out.h264",1)
+    cap = cv2.VideoCapture("rtsp://admin:admin@10.76.53.14:8554/stream0/out.h264")
     while(True):
         ret, img=cap.read()
         timenow =datetime.now() + timedelta(hours=7)
