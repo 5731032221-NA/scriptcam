3400c123103acfe3446035e89b653d031d453ad2
msc-dts <naphol159@gmail.com>
2020-06-30 15:11:24 +0700

    a

--- sc1v2.py
+++ sc1v2.py
@@ -529,7 +529,7 @@ def imagescan(frame, count,now):
                             conf = prof['individual_confidence']
                             if(identify[index][u'candidates'][0][u'confidence'] > conf):
                                 person=requests.get(uriPerson,  headers = header)
-                                print(person)
+                                print(person.json())
                                 nameperson=person.json()[u'name']
                                 mongodetect(now,now.strftime("%H:%M"), nameperson, now.strftime("%H:%M"), detect[index][u'faceAttributes'], detect[index][u'faceRectangle'], (
                                 "https://oneteamblob.blob.core.windows.net/facedetection/"+name), name_crop)
@@ -544,7 +544,7 @@ def imagescan(frame, count,now):
                                 # "https://oneteamblob.blob.core.windows.net/facedetection/"+name), name_crop)
                                 # infocrop(name_crop,now,"",0)
                                 person=requests.get(uriPerson,  headers = header)
-                                print(person)
+                                print(person.json())
                                 nameperson=person.json()[u'name']
                                 infocrop(name_crop,now,nameperson,identify[index][u'candidates'][0][u'confidence'])  
                             os.remove("data/"+name_crop)
@@ -572,7 +572,7 @@ def imagescan(frame, count,now):
                                 conf = prof['individual_confidence']
                                 if(identify[index][u'candidates'][0][u'confidence'] > conf):
                                     person=requests.get(uriPerson,  headers = header)
-                                    print(person)
+                                    print(person.json())
                                     nameperson=person.json()[u'name']
                                     mongodetect2(now,now.strftime("%H:%M"), nameperson, now.strftime("%H:%M"), detect[index][u'faceRectangle'], (
                                     "https://oneteamblob.blob.core.windows.net/facedetection/"+name), name_crop)
@@ -585,7 +585,7 @@ def imagescan(frame, count,now):
                                 else:
                                     # infocrop(name_crop,now,"",0) 
                                     person=requests.get(uriPerson,  headers = header)
-                                    print(person)
+                                    print(person.json())
                                     nameperson=person.json()[u'name']
                                     infocrop(name_crop,now,nameperson,identify[index][u'candidates'][0][u'confidence']) 
                                 os.remove("data/"+name_crop)
